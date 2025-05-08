a00c31caf38b0b1ad1775224368e06a25ecc6dee
vanhuugiacuong <cuongtomato174@gmal.com>
2025-05-08 14:16:40 +0700

    Secone

--- lib/models/book_model.dart
+++ lib/models/book_model.dart
@@ -23,7 +23,6 @@ class Book {
       publicationDate = '',
       isbn = '',
       imageUrl = '';
-      
 
   Map<String, dynamic> toJson() {
     return {
@@ -45,4 +44,69 @@ class Book {
     isbn: json['isbn'],
     imageUrl: json['imageUrl'],
   );
+}
+
+class BookList extends Book {
+  int id;
+  bool isReading;
+  double progress;
+
+  BookList({
+    required this.id,
+    required this.isReading,
+    required this.progress,
+    required String title,
+    required String author,
+    required String genre,
+    required String publicationDate,
+    required String isbn,
+    required String imageUrl,
+  }) : super(
+         title: title,
+         author: author,
+         genre: genre,
+         publicationDate: publicationDate,
+         isbn: isbn,
+         imageUrl: imageUrl,
+       );
+  // Tạo từ JSON
+  factory BookList.fromJson(Map<String, dynamic> json) => BookList(
+    id: json['id'],
+    title: json['title'],
+    author: json['author'],
+    genre: json['genre'],
+    publicationDate: json['publicationDate'],
+    isbn: json['isbn'],
+    imageUrl: json['imageUrl'],
+    isReading: json['isReading'] ?? false,
+    progress: (json['progress'] ?? 0.0).toDouble(),
+  );
+
+  @override
+  Map<String, dynamic> toJson() {
+    final json = super.toJson();
+    json['id'] = id;
+    json['isReading'] = isReading;
+    json['progress'] = progress;
+    return json;
+  }
+
+  // Đánh dấu sách là đang đọc
+  void startReading() {
+    isReading = true;
+    progress = 0.0; // Bắt đầu từ đầu
+  }
+
+  // Cập nhật tiến độ đọc
+  void updateProgress(double newProgress) {
+    if (newProgress >= 0.0 && newProgress <= 1.0) {
+      progress = newProgress;
+    }
+  }
+
+  // Đánh dấu sách là đã đọc xong
+  void finishReading() {
+    isReading = false;
+    progress = 1.0; // Đặt tiến độ là 100%
+  }
 }
